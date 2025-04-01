8c65f86040709a30c1b0f43cc408f9c30ae0fe73
Adoniran <adoniranfranca@gmail.com>
2025-04-01 15:57:14 -0300

    Adiciona sass para btns iniciais

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,11 +3,42 @@
 </script>
 
 <template>
-  <div>
-    <h1>Home Page</h1>
+  <div class="home">
+    <button>Add to Cart</button>
   </div>
 </template>
 
 <style lang="scss" scoped>
 
+.btn {
+  &.primary {
+    background-color: #98fff6;
+    color: #fff;
+
+    &:hover {
+      background-color: #fff;
+      color: #000;
+    }
+  }
+
+  &.sucess {
+    background-color: #77ff77;
+    color: #000;
+
+    &:hover {
+      background-color: #8ef98e;
+      color: #000;
+    }
+  }
+
+  &.error {
+    background-color: #ff7777;
+    color: #fff;
+
+    &.hover {
+      background-color: #fa9b9b;
+      color: #000;
+    }
+  }
+}
 </style>
